fd89706748141d472cfbfd9b60508b0d2aae38d1
Jimmy King <jimmy.king@aexp.com>
2017-11-13 08:55:44 -0700

    fix(jest-setup): polyfill order was wrong so it didnt work

--- jest-setup.js
+++ jest-setup.js
@@ -1,9 +1,10 @@
 require('amex-jest-preset/jest-setup');
-const Enzyme = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16');
-
-Enzyme.configure({ adapter: new Adapter() });
 
 global.requestAnimationFrame = (callback) => {
   setTimeout(callback, 0);
 };
+
+const Enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
+
+Enzyme.configure({ adapter: new Adapter() });
